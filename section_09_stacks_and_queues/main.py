aa3f92de6bab030f590d5cab661a6cf73b7c6a00
hankehly <henry.ehly@gmail.com>
2023-01-14 18:29:31 +0900

    Refactor StackBuiltWithArray

--- section_09_stacks_and_queues/main.py
+++ section_09_stacks_and_queues/main.py
@@ -20,35 +20,35 @@ class StackBuiltWithLinkedList:
     You could instantiate a doubly linked list from section 8 and reuse the methods here,
     but all we really need is the "Node" class which stores value, next, and prev values.
 
-    self.top and self.bottom are our "head" and "tail"
+    self._top and self._bottom are our "head" and "tail"
     """
 
     def __init__(self) -> None:
-        self.top: Node = None
-        self.bottom: Node = None
-        self.length = 0
+        self._top: Node = None
+        self._bottom: Node = None
+        self._length = 0
 
     @property
     def empty(self) -> bool:
-        return self.length == 0
+        return self._length == 0
 
     def peek(self) -> Any:
-        return self.top.value
+        return self._top.value
 
     def push(self, value: Any) -> None:
         node = Node(value)
         if self.empty:
-            self.top = node
-            self.bottom = node
-            self.length = 1
+            self._top = node
+            self._bottom = node
+            self._length = 1
         else:
             # place the new node at the front of the stack
             # and link it to the next element
-            old_top = self.top
+            old_top = self._top
             old_top.prev = node
             node.next = old_top
-            self.top = node
-            self.length += 1
+            self._top = node
+            self._length += 1
 
     def pop(self) -> Any:
         if self.empty:
@@ -56,58 +56,44 @@ class StackBuiltWithLinkedList:
         # If this stack only has 1 item, the top and bottom pointers will
         # both be null at the end of the operation. We update top below,
         # so update bottom here.
-        if self.top == self.bottom:
-            self.bottom = None
-        old_top = self.top
-        self.top = self.top.next
-        if self.top:
-            self.top.prev = None
+        if self._top == self._bottom:
+            self._bottom = None
+        old_top = self._top
+        self._top = self._top.next
+        if self._top:
+            self._top.prev = None
         # We could also update the bottom here, because if top is None,
         # bottom should also be None
         # else:
-        #     self.bottom = None
-        self.length -= 1
+        #     self._bottom = None
+        self._length -= 1
         return old_top.value
 
 
 class StackBuiltWithArray:
     def __init__(self) -> None:
-        self.data = []
+        self._data = []
 
     @property
     def empty(self) -> bool:
-        return self.length == 0
-
-    @property
-    def length(self) -> int:
-        return len(self.data)
-
-    @property
-    def top(self) -> Node:
-        if self.empty:
-            return None
-        return self.data[-1]
-
-    @property
-    def bottom(self) -> Node:
-        if self.empty:
-            return None
-        return self.data[0]
+        return len(self._data) == 0
 
     def peek(self) -> Any:
-        return self.top.value
+        if self.empty:
+            return None
+        return self._data[-1].value
 
     def push(self, value: Any) -> None:
-        logging.debug(f"[push] Pushing {value} on the stack")
+        logging.debug(f"[push] Pushing {value} onto the stack.")
         new_node = Node(value)
-        self.data.append(new_node)
+        self._data.append(new_node)
 
     def pop(self) -> Any:
         if self.empty:
             return None
-        node = self.data.pop()
+        node = self._data.pop()
         logging.debug(
-            f"[pop] Popped {node.value} from stack. Length is now {len(self.data)}"
+            f"[pop] Popped {node.value} from the stack. Length is now {len(self._data)}."
         )
         return node.value
 
@@ -119,17 +105,12 @@ def main():
     stack.push("udemy")
     stack.push("discord")
     assert not stack.empty
-    assert stack.top.value == "discord"
-    assert stack.bottom.value == "google"
     assert stack.peek() == "discord"
     assert stack.pop() == "discord"
     assert stack.pop() == "udemy"
-    assert stack.top.value == "google"
     assert stack.pop() == "google"
     assert stack.empty
     assert stack.pop() is None
-    assert stack.top is None
-    assert stack.bottom is None
 
 
 if __name__ == "__main__":
