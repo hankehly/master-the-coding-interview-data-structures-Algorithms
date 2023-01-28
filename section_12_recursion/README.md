0573c1a7c710a3be60dc1366437c661efdc95a8a
hankehly <henry.ehly@gmail.com>
2023-01-28 15:04:36 +0900

    Add fibonacci functions

--- section_12_recursion/README.md
+++ section_12_recursion/README.md
@@ -6,3 +6,23 @@ Recursive Case | The case in a recursive function where you call the function ag
 Base Case | The case in a recursive function where you stop calling the function
 
 Usually you have 2 returns – one for the base case and one for the recursive case.
+
+### Figuring out the recursive case
+
+When you're trying to figure out the recursive case, try to explain the answer by using the function itself. For example:
+```
+What's the value of f(5) ?
+It's f(3) + f(4)                 # Use the function itself to explain the answer
+Okay, what's the value of f(4)?  # Ask the question repeatedly and it becomes apparent how the recursion should work
+It's f(2) + f(3)
+...
+```
+
+Continue until you reach low numbers (nearing the base case). It should become apparent that the answer to a certain prompt is always going to be a specific value, like 1 or something.
+
+```
+What's the value of f(2) ?
+It's 1, always               # This is your base case (if n==1)
+What's the value of f(1) ?
+It's 0, always               # This can also be part of the base case (if n==0)
+```
--- section_12_recursion/main.py
+++ section_12_recursion/main.py
@@ -30,8 +30,47 @@ def find_factorial_iterative(n: int) -> int:
     return answer
 
 
+def fibonacci_recursive(n) -> int:
+    """
+    Return the n-th value of the Fibonacci sequence.
+      0, 1, 1, 2, 3, 5, 8, 13, 21, ..
+
+    Example:
+      n=2, answer=1
+      n=3, answer=2
+      n=7, answer=13
+    """
+    assert n >= 0
+    if n < 2:
+        return n
+    else:
+        # What's the value of fib(3)?
+        # It's fib(1) + fib(2)
+        # Okay, what's the value of fib(4)?
+        # Well, it's fib(2) + fib(3)
+        return fibonacci_recursive(n - 2) + fibonacci_recursive(n - 1)
+
+
+def fibonacci_iterative(n) -> int:
+    assert n >= 0
+    if n < 2:
+        return n
+    fib = [0, 1]
+    # Ignore the first 2 indices, start from fib[2] and go until n (n+1 so that n is included)
+    for i in range(2, n + 1):
+        fib.append(fib[i - 2] + fib[i - 1])
+    # Now that we have n numbers in the sequence, return the one we need
+    return fib[n]
+
+
 if __name__ == "__main__":
+    # Factorial
     assert find_factorial_recursive(1) == find_factorial_iterative(1) == 1
     assert find_factorial_recursive(2) == find_factorial_iterative(2) == 2
     assert find_factorial_recursive(3) == find_factorial_iterative(3) == 6
     assert find_factorial_recursive(4) == find_factorial_iterative(4) == 24
+    # Fibonacci
+    assert fibonacci_recursive(3) == fibonacci_iterative(3) == 2
+    assert fibonacci_recursive(2) == fibonacci_iterative(2) == 1
+    assert fibonacci_recursive(7) == fibonacci_iterative(7) == 13
+    assert fibonacci_recursive(8) == fibonacci_iterative(8) == 21
